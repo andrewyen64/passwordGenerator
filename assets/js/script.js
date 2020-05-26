e20923869a4ca26d201465b055d7de602028ee5a
Andrew <andrewyen64@gmail.com>
2020-05-26 11:18:42 -0700

    fix function generatePassword by moving var password, passwordText to the top of the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,58 +28,45 @@ function writePassword() {
   var lowercase = confirm("Would you like to include lowercase letters?");  
   if (lowercase === true) {
     result = result.concat(lowerList);
-    console.log(result);
   }
   // Confirm Uppercase
   var uppercase = confirm("Would you like to include uppercase letters?");
   if (uppercase === true) {
     result = result.concat(upperList);
-    console.log(result);
   }
   // Confirm Numbers
   var number = confirm("Would you like to include numbers?");
   if (number === true) {
     result = result.concat(numberList);
-    console.log(result);
   }
 
   // Confirm Special Characters
   var special = confirm("Would you like special characters?");
   if (special === true) {
     result = result.concat(specialList);
-    console.log(result);
   }
 
-
   // you can create a function named generatePassword that creates the password
+  // var password = '';
   function generatePassword() {
+    var password = '';
+    var passwordText = document.querySelector("#password");
+
     if (lowercase === false && uppercase === false && 
       number === false && special === false) {
         alert ("You must select at least one character type!");
-      } else {
+      } else {        
         for (var i = 0; i < charLength; i++) {
-          password = result[Math.floor(Math.random() * result.length) + 1];
-          console.log(password);
-          var passwordText = document.querySelector("#password");
-          passwordText.value = password;
+          password += result[Math.floor(Math.random() * result.length) + 1];
         }
-         
+        passwordText.value = password;
       }
       
   }
 
   var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-  // for (var i = 0; i < charLength; i++) {
-  //   result.push();
-  //   console.log(result);
-  // }
-
-  // result.join('');
